7ba190b4fe6118472b5f013c209e57841bf9f3d7
Ali Ghahraei <aligf94@gmail.com>
2022-01-25 02:32:13 -0600

    Write new project to a dot file

--- route_tracker/tracker.py
+++ route_tracker/tracker.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from pydot import Dot
 from typer import Typer, echo
+from xdg import xdg_data_home
 
 app = Typer()
 
 
 @app.command()
 def new(name: str) -> None:
+    data_dir = xdg_data_home() / 'route-tracker'
+    data_dir.mkdir(exist_ok=True)
+    Dot(name, graph_type='graph').write_dot(data_dir / name)
     echo(f'{name} created')
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,23 +1,58 @@
 #!/usr/bin/env python3
+import os
+from pathlib import Path
+from textwrap import dedent
+from typing import Callable
+
+from click.testing import Result
 from pytest import fixture
 from typer.testing import CliRunner
 
 from route_tracker.tracker import app
 
+CommandRunner = Callable[[], Result]
+
 
 @fixture
-def runner() -> CliRunner:
+def cli_runner() -> CliRunner:
     return CliRunner()
+
+
+@fixture(autouse=True)
+def test_data_dir(tmp_path: Path) -> Path:
+    data_dir = tmp_path
+    os.environ['XDG_DATA_HOME'] = str(data_dir)
+    return data_dir
 
 
 class TestNewCommand:
     @staticmethod
-    def test_new_exits_normally_when_called_with_name(runner: CliRunner) \
-            -> None:
-        assert runner.invoke(app, ['test_name']).exit_code == 0
+    @fixture
+    def command_runner(cli_runner: CliRunner) -> CommandRunner:
+        return lambda: cli_runner.invoke(app, ['test_name'])
+
+    @staticmethod
+    def test_new_exits_normally_when_called_with_name(
+            command_runner: CommandRunner,
+    ) -> None:
+        assert command_runner().exit_code == 0
 
     @staticmethod
     def test_new_shows_project_created_when_called_with_name(
-            runner: CliRunner,
+            command_runner: CommandRunner,
     ) -> None:
-        assert 'test_name created' in runner.invoke(app, ['test_name']).stdout
+        assert 'test_name created' in command_runner().stdout
+
+    @staticmethod
+    def test_new_creates_dot_file_when_called_with_name(
+            command_runner: CommandRunner, test_data_dir: Path,
+    ) -> None:
+        command_runner()
+        with open(test_data_dir / 'route-tracker' / 'test_name') as f:
+            expected = dedent("""\
+            graph test_name {
+            \tgraph [bb="0,0,0,0"];
+            \tnode [label="\\N"];
+            }
+            """)
+            assert f.read() == expected
