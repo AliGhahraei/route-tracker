80979e209b1ca5648a3b25d6c64b4019f68e2add
Ali Ghahraei <aligf94@gmail.com>
2022-01-26 02:20:24 -0600

    Finish implementing the add command

--- route_tracker/tracker.py
+++ route_tracker/tracker.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
-from typing import List
+from typing import Iterable, List, Sequence, cast
 
 import pygraphviz as pgv
+from tomlkit import dumps, parse
 from typer import Exit, Typer, echo
 from xdg import xdg_data_home
 
@@ -12,28 +13,92 @@ app = Typer()
 
 @app.command()
 def new(name: str) -> None:
-    data_file = get_data_file(name)
-    if data_file.exists():
+    if _get_graph_file(name).exists():
         echo(f'{name} already exists. Ignoring...', err=True)
         raise Exit(code=1)
-    pgv.AGraph(name=name).write(data_file)
+    _create_new_graph(name)
+    _store_last_selected_choice(name, 'start')
+
+
+def _get_graph_file(name: str) -> Path:
+    return _get_project_dir(name) / 'graph'
+
+
+def _get_project_dir(name: str) -> Path:
+    data_dir = xdg_data_home() / 'route-tracker' / name
+    data_dir.mkdir(parents=True, exist_ok=True)
+    return data_dir
+
+
+def _create_new_graph(name: str) -> None:
+    graph = pgv.AGraph(name=name)
+    graph.add_node('start', label='start')
+    graph.write(_get_graph_file(name))
     echo(f'{name} created')
+
+
+def _store_last_selected_choice(project_name: str, choice: str) -> None:
+    with open(_get_project_dir(project_name) / 'data', 'w+') as f:
+        doc = parse(f.read())
+        doc['last_selected_choice'] = choice
+        f.write(dumps(doc))
 
 
 @app.command()
 def add(project_name: str) -> None:
+    _get_graph(project_name)
+    choices = _read_choices()
+    selected_choice = _get_selected_choice(choices)
+    graph = _add_choices_to_graph(choices, project_name)
+    graph.write(_get_graph_file(project_name))
+    _store_last_selected_choice(project_name, selected_choice)
+
+
+def _get_graph(name: str) -> pgv.AGraph:
+    try:
+        graph = pgv.AGraph(_get_graph_file(name))
+    except FileNotFoundError:
+        echo(f'Project {name} does not exist', err=True)
+        raise Exit(code=1)
+    return graph
+
+
+def _read_choices() -> List[str]:
+    echo('Enter available choices separated by newlines. A blank line ends the'
+         ' input')
     choices: List[str] = []
     while (line := sys.stdin.readline()) != '\n':
         choices.append(line.rstrip())
+    if not choices:
+        echo('At least one choice must be entered', err=True)
+        raise Exit(code=1)
+    return choices
 
-    data_file = get_data_file(project_name)
-    graph = pgv.AGraph(data_file)
+
+def _get_selected_choice(choices: Sequence[str]) -> str:
+    index_input = input('Enter the zero-based index of your selection\n')
+    try:
+        selected_choice = choices[int(index_input)]
+    except ValueError:
+        echo(f'Index {index_input} is not a number', err=True)
+        raise Exit(code=1)
+    except IndexError:
+        echo(f'Index {index_input} is out of bounds', err=True)
+        raise Exit(code=1)
+    return selected_choice
+
+
+def _add_choices_to_graph(choices: Iterable[str], project_name: str) \
+        -> pgv.AGraph:
+    graph = _get_graph(project_name)
+    last_selected_choice = _get_last_selected_choice(project_name)
     for choice in choices:
         graph.add_node(choice, label=choice)
-    graph.write(data_file)
+        graph.add_edge(last_selected_choice, choice)
+    return graph
 
 
-def get_data_file(name: str) -> Path:
-    data_dir = xdg_data_home() / 'route-tracker'
-    data_dir.mkdir(exist_ok=True)
-    return data_dir / name
+def _get_last_selected_choice(name: str) -> str:
+    with open(_get_project_dir(name) / 'data') as f:
+        config = parse(f.read())
+        return cast(str, config['last_selected_choice'])
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 import os
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Protocol
 
 import pygraphviz as pgv
 from click.testing import Result
-from pytest import fixture
+from pytest import fixture, mark
 from typer.testing import CliRunner
 
 from route_tracker.tracker import app
 
-CommandRunner = Callable[[], Result]
+AddRunner = Callable[[str], Result]
+
+
+class NewRunner(Protocol):
+    def __call__(self, project_name: str = ...) -> Result:
+        pass
 
 
 @fixture
@@ -26,55 +31,154 @@ def test_data_dir(tmp_path: Path) -> Path:
 
 
 @fixture
-def new_command_runner(cli_runner: CliRunner) -> CommandRunner:
-    return lambda: cli_runner.invoke(app, ['new', 'test_name'])
+def new_runner(cli_runner: CliRunner) -> NewRunner:
+    def runner(project_name: str = 'test_name') -> Result:
+        return cli_runner.invoke(app, ['new', project_name])
+    return runner
 
 
+@fixture
+def starting_graph() -> pgv.AGraph:
+    graph = pgv.AGraph(name='test_name')
+    graph.add_node('start', label='start')
+    return graph
+
+
+def assert_graphs_equal(data_dir: Path, expected_graph: pgv.AGraph) -> None:
+    graph = pgv.AGraph(data_dir / 'route-tracker' / 'test_name' / 'graph')
+    assert graph.to_string() == expected_graph.to_string()
+
+
+def assert_normal_exit(result: Result, message: str) -> None:
+    assert message in result.stdout
+    assert result.exit_code == 0
+
+
+def assert_error_exit(result: Result, message: str) -> None:
+    assert message in result.stderr
+    assert result.exit_code == 1
 
 
 class TestNewCommand:
     @staticmethod
     def test_new_exits_normally_when_called_with_name(
-            new_command_runner: CommandRunner,
+            new_runner: NewRunner,
     ) -> None:
-        result = new_command_runner()
-        assert 'test_name created' in result.stdout
-        assert result.exit_code == 0
+        assert_normal_exit(new_runner(), 'test_name created')
 
     @staticmethod
     def test_new_creates_dot_file_when_called_with_name(
-            new_command_runner: CommandRunner, test_data_dir: Path,
+            new_runner: NewRunner, test_data_dir: Path,
+            starting_graph: pgv.AGraph,
     ) -> None:
-        new_command_runner()
-        assert (pgv.AGraph(test_data_dir / 'route-tracker' / 'test_name')
-                == pgv.AGraph(name='test_name'))
+        new_runner()
+        assert_graphs_equal(test_data_dir, starting_graph)
 
     @staticmethod
     def test_new_exits_with_error_when_called_with_same_name_twice(
-            new_command_runner: CommandRunner, test_data_dir: Path,
+            new_runner: NewRunner, test_data_dir: Path,
     ) -> None:
-        new_command_runner()
-        result = new_command_runner()
-        assert 'test_name already exists. Ignoring...' in result.stderr
-        assert result.exit_code == 1
+        new_runner()
+        assert_error_exit(new_runner(),
+                          'test_name already exists. Ignoring...')
+
+    @staticmethod
+    def test_new_exits_normally_when_called_with_different_names(
+            new_runner: NewRunner,
+    ) -> None:
+        assert_normal_exit(new_runner(), 'test_name created')
+        assert_normal_exit(new_runner('another_name'), 'another_name created')
 
 
 class TestAddCommand:
     @staticmethod
     @fixture
-    def add_command_runner(cli_runner: CliRunner) -> CommandRunner:
-        return lambda: cli_runner.invoke(app, ['add', 'test_name'],
-                                         input='choice1\n\n')
+    def add_runner(cli_runner: CliRunner) -> AddRunner:
+        return lambda input_: cli_runner.invoke(app, ['add', 'test_name'],
+                                                input=input_)
 
     @staticmethod
-    def test_add_creates_node_when_called_with_name(
-            new_command_runner: CommandRunner,
-            add_command_runner: CommandRunner, test_data_dir: Path,
+    def test_add_exits_normally_when_called_with_choices(
+            new_runner: NewRunner, add_runner: AddRunner,
     ) -> None:
-        new_command_runner()
-        add_command_runner()
+        new_runner()
+        assert_normal_exit(add_runner('choice1\n\n0'),
+                           'Enter available choices separated by newlines. A'
+                           ' blank line ends the input\nEnter the zero-based'
+                           ' index of your selection')
 
-        expected = pgv.AGraph(name='test_name')
+    @staticmethod
+    def test_add_saves_single_choice_when_called_with_single_choice(
+            new_runner: NewRunner, starting_graph: pgv.AGraph,
+            add_runner: AddRunner, test_data_dir: Path,
+    ) -> None:
+        new_runner()
+        add_runner('choice1\n\n0')
+
+        expected = starting_graph
         expected.add_node('choice1', label='choice1')
-        assert (pgv.AGraph(test_data_dir / 'route-tracker' / 'test_name')
-                == expected)
+        expected.add_edge('start', 'choice1')
+        assert_graphs_equal(test_data_dir, expected)
+
+    @staticmethod
+    def test_add_saves_choices_when_called_with_multiple_choices(
+            new_runner: NewRunner, starting_graph: pgv.AGraph,
+            add_runner: AddRunner, test_data_dir: Path,
+    ) -> None:
+        new_runner()
+        add_runner('choice1\nchoice2\n\n1')
+
+        expected = starting_graph
+        expected.add_node('choice1', label='choice1')
+        expected.add_edge('start', 'choice1')
+        expected.add_node('choice2', label='choice2')
+        expected.add_edge('start', 'choice2')
+        assert_graphs_equal(test_data_dir, expected)
+
+    @staticmethod
+    def test_add_saves_choices_when_called_with_multiple_add_commands(
+            new_runner: NewRunner, starting_graph: pgv.AGraph,
+            add_runner: AddRunner, test_data_dir: Path,
+    ) -> None:
+        new_runner()
+        add_runner('choice1\n\n0')
+        add_runner('choice2\n\n0')
+
+        expected = starting_graph
+        expected.add_node('choice1', label='choice1')
+        expected.add_edge('start', 'choice1')
+        expected.add_node('choice2', label='choice2')
+        expected.add_edge('choice1', 'choice2')
+        assert_graphs_equal(test_data_dir, expected)
+
+    @staticmethod
+    def test_add_exits_with_error_when_called_with_no_choices(
+            new_runner: NewRunner, add_runner: AddRunner,
+    ) -> None:
+        new_runner()
+        assert_error_exit(add_runner('\n'),
+                          'At least one choice must be entered')
+
+    @staticmethod
+    @mark.parametrize('index', [1, 2, -2])
+    def test_add_exits_with_error_when_selected_choice_is_out_of_bounds(
+            index: int, new_runner: NewRunner, add_runner: AddRunner,
+    ) -> None:
+        new_runner()
+        assert_error_exit(add_runner(f'choice1\n\n{index}'),
+                          f'Index {index} is out of bounds')
+
+    @staticmethod
+    def test_add_exits_with_error_when_selected_choice_is_not_a_number(
+            new_runner: NewRunner, add_runner: AddRunner,
+    ) -> None:
+        new_runner()
+        assert_error_exit(add_runner('choice1\n\nnot_a_number'),
+                          'Index not_a_number is not a number')
+
+    @staticmethod
+    def test_add_aborts_if_project_does_not_exist(
+            add_runner: AddRunner,
+    ) -> None:
+        assert_error_exit(add_runner('choice\n\n'),
+                          'Project test_name does not exist')
