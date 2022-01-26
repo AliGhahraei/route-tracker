b5d96b468d9c1bc82132fe2d6ef8ffe8e99aa143
Ali Ghahraei <aligf94@gmail.com>
2022-01-25 18:47:57 -0600

    Implement basic add functionality

--- route_tracker/tracker.py
+++ route_tracker/tracker.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-from pydot import Dot
+import sys
+from pathlib import Path
+from typing import List
+
+from pydot import Dot, Node, graph_from_dot_file
 from typer import Exit, Typer, echo
 from xdg import xdg_data_home
 
@@ -8,11 +12,28 @@ app = Typer()
 
 @app.command()
 def new(name: str) -> None:
-    data_dir = xdg_data_home() / 'route-tracker'
-    data_dir.mkdir(exist_ok=True)
-    data_file = data_dir / name
+    data_file = get_data_file(name)
     if data_file.exists():
         echo(f'{name} already exists. Ignoring...', err=True)
         raise Exit(code=1)
     Dot(name, graph_type='graph').write_dot(data_file)
     echo(f'{name} created')
+
+
+@app.command()
+def add(project_name: str) -> None:
+    choices: List[str] = []
+    while (line := sys.stdin.readline()) != '\n':
+        choices.append(line.rstrip())
+
+    data_file = get_data_file(project_name)
+    graph = graph_from_dot_file(data_file)[0]
+    for choice in choices:
+        graph.add_node(Node(choice, label=choice))
+    graph.write_dot(data_file)
+
+
+def get_data_file(name: str) -> Path:
+    data_dir = xdg_data_home() / 'route-tracker'
+    data_dir.mkdir(exist_ok=True)
+    return data_dir / name
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from pathlib import Path
-from textwrap import dedent
+from string import punctuation, whitespace
 from typing import Callable
 
 from click.testing import Result
@@ -25,39 +25,76 @@ def test_data_dir(tmp_path: Path) -> Path:
     return data_dir
 
 
+@fixture
+def new_command_runner(cli_runner: CliRunner) -> CommandRunner:
+    return lambda: cli_runner.invoke(app, ['new', 'test_name'])
+
+
+def assert_graphs_equal(graph1: str, graph2: str) -> None:
+    removal_table = str.maketrans(dict.fromkeys(punctuation + whitespace))
+    assert graph1.translate(removal_table) == graph2.translate(removal_table)
+
+
 class TestNewCommand:
     @staticmethod
-    @fixture
-    def command_runner(cli_runner: CliRunner) -> CommandRunner:
-        return lambda: cli_runner.invoke(app, ['test_name'])
-
-    @staticmethod
     def test_new_exits_normally_when_called_with_name(
-            command_runner: CommandRunner,
+            new_command_runner: CommandRunner,
     ) -> None:
-        result = command_runner()
+        result = new_command_runner()
         assert 'test_name created' in result.stdout
         assert result.exit_code == 0
 
     @staticmethod
     def test_new_creates_dot_file_when_called_with_name(
-            command_runner: CommandRunner, test_data_dir: Path,
+            new_command_runner: CommandRunner, test_data_dir: Path,
     ) -> None:
-        command_runner()
+        new_command_runner()
         with open(test_data_dir / 'route-tracker' / 'test_name') as f:
-            expected = dedent("""\
+            expected = """
             graph test_name {
-            \tgraph [bb="0,0,0,0"];
-            \tnode [label="\\N"];
+                graph [bb="0,0,0,0"];
+                node [label="\\N"];
             }
-            """)
-            assert f.read() == expected
+            """
+            assert_graphs_equal(f.read(), expected)
 
     @staticmethod
     def test_new_exits_with_error_when_called_with_same_name_twice(
-            command_runner: CommandRunner, test_data_dir: Path,
+            new_command_runner: CommandRunner, test_data_dir: Path,
     ) -> None:
-        command_runner()
-        result = command_runner()
+        new_command_runner()
+        result = new_command_runner()
         assert 'test_name already exists. Ignoring...' in result.stderr
         assert result.exit_code == 1
+
+
+class TestAddCommand:
+    @staticmethod
+    @fixture
+    def add_command_runner(cli_runner: CliRunner) -> CommandRunner:
+        return lambda: cli_runner.invoke(app, ['add', 'test_name'],
+                                         input='choice1\n\n')
+
+    @staticmethod
+    def test_add_creates_node_when_called_with_name(
+            new_command_runner: CommandRunner,
+            add_command_runner: CommandRunner, test_data_dir: Path,
+    ) -> None:
+        new_command_runner()
+        add_command_runner()
+
+        with open(test_data_dir / 'route-tracker' / 'test_name') as f:
+            expected = """
+            graph test_name {
+                graph [bb="0,0,148.35,36"];
+                node [label="\\N"];
+                "\\n" [height=0.5,
+                      pos="27,18",
+                      width=0.75];
+                choice1 [height=0.5,
+                         label=choice1,
+                         pos="110,18",
+                         width=1.0652];
+            }
+            """
+            assert_graphs_equal(expected, f.read())
